ce9ce0b306feea3e6eff7a919905dd51e33e28c0
MrCliff <j_1.kallio@hotmail.com>
2020-11-24 21:22:17 +0200

    Add inputs and ground sprite
    * Setup sprite shape for ground.
    * Add controls for moving and jumping.
    * Add camera controller script.

---
+++ Joulupeli-2020/Assets/Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    /// <summary>
+    /// Controls the camera :D
+    /// </summary>
+    public class CameraController : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform targetToFollow;
+
+        [SerializeField]
+        [Min(0)]
+        private float speed = 100;
+
+        private void FixedUpdate()
+        {
+            if (targetToFollow == null)
+            {
+                return;
+            }
+
+            Vector3 newCameraPosition = Vector3.Lerp(transform.position, targetToFollow.position, speed * Time.fixedDeltaTime);
+            newCameraPosition.z = transform.position.z;
+            transform.position = newCameraPosition;
+        }
+    }
+}
---
+++ Joulupeli-2020/Assets/Scripts/CarEngine.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Assets.Scripts
+{
+    public class CarEngine : MonoBehaviour
+    {
+        [SerializeField]
+        private WheelJoint2D frontWheel;
+
+        [SerializeField]
+        private WheelJoint2D rearWheel;
+
+        [SerializeField]
+        [Range(0, 1)]
+        private float moveThreshold = 0;
+
+        [SerializeField]
+        [Min(0)]
+        private float motorSpeed = 1000;
+
+        [SerializeField]
+        private float jumpForceMagnitude = 100;
+
+        private void Awake()
+        {
+            if (frontWheel == null)
+            {
+                Debug.LogError("No front wheel attached to car engine.");
+            }
+            if (rearWheel == null)
+            {
+                Debug.LogError("No rear wheel attached to car engine.");
+            }
+        }
+
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            Vector2 direction = context.ReadValue<Vector2>();
+            int directionModifier = 0;
+            if (direction.x > moveThreshold) directionModifier = -1;
+            if (direction.x < -moveThreshold) directionModifier = 1;
+
+            JointMotor2D frontWheelMotor = frontWheel.motor;
+            frontWheelMotor.motorSpeed = directionModifier * motorSpeed;
+            frontWheel.motor = frontWheelMotor;
+            frontWheel.useMotor = directionModifier != 0;
+        }
+
+        public void OnJump(InputAction.CallbackContext context)
+        {
+            Vector2 jumpForce = new Vector2(0, jumpForceMagnitude);
+
+            frontWheel.attachedRigidbody.AddForce(jumpForce, ForceMode2D.Impulse);
+            rearWheel.attachedRigidbody.AddForce(jumpForce, ForceMode2D.Impulse);
+        }
+    }
+}
